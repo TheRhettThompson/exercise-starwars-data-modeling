7e8543fdcf415d65b2fe5e0619dbcac167baad32
TheRhettThompson <rthompsoncareer@gmail.com>
2022-12-03 02:34:36 +0000

    added characters planets vehicles

--- src/models.py
+++ src/models.py
@@ -8,23 +8,47 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(250) , nullable=False)
+    password = Column(String(250) , nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer , primary_key=True)
+    character_name = Column(String(250))
+    planet_from = Column(String(250) , ForeignKey('planets.name'))
+    age = Column(Integer)
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    size = Column(Integer)
+    population = Column(Integer)
+    climate = Column(String(250))
+   
+    
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer , primary_key=True)
+    name = Column(String(250))
+    vehicle_type = Column(String(250))
+    pilot = (String(250) , ForeignKey('character.name'))
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    user_id = Column(Integer)
+    date_added = Column(Integer)
+    favorite_characters = Column(String(250) , ForeignKey('character.name'))
+    favorite_planets = Column(String(250))
+    favorite_vehicles = Column(String(250))
+
+
 
     def to_dict(self):
         return {}
